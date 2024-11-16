f31663b6fdee5fbeb33e64b0465f6b46e3fee4c9
Đức Việt <ducviettk099@gmail.com>
2024-11-16 15:16:36 +0700

    n

--- src/app/(users)/components/pro.jsx
+++ src/app/(users)/components/pro.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import ProductCard from "@/app/(users)/components/ProductCard";
+import ProductCard from "./ProductCard";
 
 export default function Pro({ key }) {
   const [data, setProduct] = useState([]); // Initialize as an empty array
